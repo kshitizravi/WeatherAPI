49a327f84a346b6b11ba2f665e5fd314dd1b2ad6
kshitiz ravi <kshitiz.ravi@gmail.com>
2021-01-20 14:59:44 +0530

    Master Commit

--- App.js
+++ App.js
@@ -21,6 +21,7 @@ export default class App extends React.Component{
         pressure:"",
         visibility:""
       }
+      this.fetch_weather()
   }
   fetch_weather=()=>{
     fetch("https://api.openweathermap.org/data/2.5/weather?q=Pune&appid=a8da961d4f3fb1faeee70599f53e9e82")
@@ -28,6 +29,7 @@ export default class App extends React.Component{
     .then((json=>{
       this.setState({data:json})
       this.setState({ temp : (json.main.temp-303.58).toFixed(2)+" c"})
+      this.setState({desc: json.weather[0].description})
       this.setState({city_display : json.name})
       this.setState({icon : json.weather[0].icon})
       this.setState({main : json.weather[0].main})
@@ -48,16 +50,29 @@ export default class App extends React.Component{
           placeholderTextColor="#FFF" 
           styles={styles.searchbox} 
           onChangeText={(text)=>this.setState({city:text})}/>
-          <TouchableOpacity style={styles.button}>
+          <TouchableOpacity style={styles.button} onPress={this.fetch_weather}>
             <Icon name="search1" size={24} color="#FFF" />
           </TouchableOpacity>
         </View>
         <View style={styles.appboxmain}>
           <View style={styles.HolderView}>
             <Image source={{uri:"http://openweathermap.org/img/wn"+this.state.icon+"@2x.png"}} style={styles.Weather_Image}/>
+            <View>
+              <Text style={styles.temp_text}>{this.state.temp}</Text>
+              <Text style={styles.city_text}>{this.state.city_display}</Text>
+            </View>
           </View>
         </View>
-
+          <View style={styles.Info_Box}>
+          <View style={styles.Info_Holder}>
+            <Text style={styles.weather_text}>{this.state.main}</Text>
+            <Text style={styles.desc_text}>{this.state.desc}</Text>
+            <Text style={styles.humidity_text}>{this.state.humidity}</Text>
+            <Text style={styles.other_text}>{this.state.pressure}</Text>
+            <Text style={styles.other_text}>{this.state.visibility}</Text>
+          </View> 
+          </View>
+        
         </ImageBackground>
     </SafeAreaView>
    )   
@@ -115,5 +130,56 @@ const styles = StyleSheet.create({
   Weather_Image:{
     height: "80%",
     width: "50%"
+  },
+  temperature_text:{
+    fontSize: 30,
+    color:"#FFF",
+    marginLeft: "5%"
+  },
+  city_text: {
+    fontSize: 20,
+    color:"#FFF" ,
+    marginLeft:"5%",
+    marginTop: "3%"
+  },
+
+  Info_Box:{
+    height:"45%",
+    width:"100%",
+    justifyContent:"center",
+    alignItems:"center"
+  },
+
+  Info_Holder:{
+    height:"80%",
+    width:"90%",
+    backgroundColor:"rgba(255,255,255,0.92)",
+    borderRadius:15
+  },
+
+  weather_text:{
+    fontSize:28,
+    color:"#121212",
+    marginLeft:"8%",
+    marginTop:"8%"
+  },
+  desc_text: {
+    fontSize:20,
+    color:"#121212",
+    marginLeft:"8%",
+    marginTop:"5%"
+  },
+  humidity_text:{
+    fontSize: 18,
+    color:"#121212",
+    marginLeft:"8%",
+    marginTop:"8%"
+  },
+  other_text:{
+    fontSize:18,
+    color:"#121212",
+    marginLeft:"8%",
+    marginTop:"2%"
   }
+
 })
